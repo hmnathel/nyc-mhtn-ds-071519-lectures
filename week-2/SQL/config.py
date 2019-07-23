2e1a5930272010d5964bcc6701c0722f4b53294a
Harris Nathel <hmnathel@gmail.com>
2019-07-23 16:21:23 -0400

    commit done

---
+++ week-2/SQL/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 15:16:58 2019
+
+@author: hmnathel
+"""
+
+host = 'flatiron.cilucwxmxhor.us-east-2.rds.amazonaws.com'
+username = 'hmnathel'
+password = 'Laylafrankie23'
